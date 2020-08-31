c76e436a57557082504c516cce2629e465c69b31
bolatt <webdevgoku@gmail.com>
2020-08-31 08:10:35 -0400

    add item and delete note
    issues have to manually update page to see result

--- notes/src/components/Item/Item.js
+++ notes/src/components/Item/Item.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./item.scss";
+import Axios from "axios";
 
 const Item = (props) => {
-  console.log(props);
+  const [oldData, setOldData] = useState([]);
+
+  console.log("from props", props);
   return (
     <div class="item">
       <i class="sticky note icon"></i>
       <div class="content">
-  <a class="header">{props.data.title}</a>
-  <div class="description">{props.data.desc}</div>
+        <a class="header">{props.data.title}</a>
+        <div class="description">{props.data.desc}</div>
       </div>
       <span class="right floated trash">
         <i class="trash alternate outline icon"></i>
--- notes/src/components/Note/Note.js
+++ notes/src/components/Note/Note.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Item from "../Item/Item";
 import "./note.scss";
 import Axios from "axios";
@@ -7,7 +7,13 @@ const Note = (props) => {
   console.log(props);
   const { id, title, items } = props;
   const focusInput = useRef();
+
   const [newTitle, setNewTitle] = useState(title);
+  const [show, setShow] = useState(false);
+  const [itemTitle, setItemTitle] = useState("");
+  const [itemDesc, setItemDesc] = useState("");
+  const [oldList, setOldList] = useState("");
+  const [reload, setReload] = useState(false);
 
   const editNote = () => {
     focusInput.current.focus();
@@ -17,8 +23,11 @@ const Note = (props) => {
     if (e.key === "Enter") {
       Axios.patch(`http://localhost:3000/notes/${id}`, {
         title: newTitle,
-      }).then((res) => console.log(res));
+      })
+        .then((res) => console.log(res))
+        .then(focusInput.current.blur());
     }
+    setReload(!reload);
   };
 
   const handleKeyDown = (e) => {
@@ -28,12 +37,46 @@ const Note = (props) => {
   };
 
   const renderItems = (items) => {
-    return items.map((item) => {
-      return <Item data={item} />;
+    return items.map((item, index) => {
+      return <Item data={item} noteID={id} index={index} />;
     });
   };
+
+  const deleteNote = () => {
+    Axios.delete(`http://localhost:3000/notes/${id}`).then((res) => {
+      console.log(res);
+
+      // window.location.reload();
+    });
+    setReload(!reload);
+  };
+
+  const addItem = (e) => {
+    e.preventDefault();
+
+    Axios.get(`http://localhost:3000/notes/${id}`).then((res) => {
+      console.log("res.data", res.data);
+      Axios.patch(`http://localhost:3000/notes/${id}`, {
+        ...res.data,
+        items: [
+          ...res.data.items,
+          {
+            id: itemTitle,
+            title: itemTitle,
+            desc: itemDesc,
+          },
+        ],
+      }).then((res) => console.log(res));
+    });
+    setReload(!reload);
+  };
+
+  const showAddItem = () => {
+    setShow(!show);
+  };
+
   return (
-    <div class="ui card note">
+    <div class="ui raised card note">
       <div class="content">
         <div class="header note-header">
           {/* <span class="left floated title">{title}</span> */}
@@ -62,15 +105,48 @@ const Note = (props) => {
         </div>
       </div>
       <div class="extra content">
-        <span class="left floated plus add-item">
+        <span class="left floated plus add-item" onClick={showAddItem}>
           <i class="plus icon"></i>
           Add Item
         </span>
-        <span class="right floated delete note-delete">
+
+        <span onClick={deleteNote} class="right floated delete note-delete">
           <i class="trash alternate outline icon"></i>
           Delete Note
         </span>
       </div>
+
+      {/* add item */}
+      {show && (
+        <div class="content">
+          <form onSubmit={(e) => addItem(e)}>
+            <div class="ui input" style={{ width: "100%" }}>
+              <input
+                type="text"
+                value={itemTitle}
+                onChange={(e) => setItemTitle(e.target.value)}
+                placeholder="Item Title... "
+              />
+            </div>
+
+            <div class="ui input" style={{ width: "100%" }}>
+              <input
+                type="text"
+                value={itemDesc}
+                onChange={(e) => setItemDesc(e.target.value)}
+                placeholder="Item Description... "
+              />
+            </div>
+            <button
+              class="ui icon button"
+              style={{ width: "100%" }}
+              type="submit"
+            >
+              <i class="add icon"></i>
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
